f8944eb917c04b06c1a3a7b1238d6cad9c1ecae0
paramjeet <khatripp786@gmail.com>
2025-02-21 13:43:22 +0530

    Updated CV

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,38 +1,44 @@
-import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/avator16.png';
-import sun from '../../assets/sun.svg';
-import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
-import { useTheme } from '../../common/ThemeContext';
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/avator18.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import CV from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
+import { Typewriter } from "react-simple-typewriter";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
-
-  const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
-  const githubIcon = theme === 'light' ? githubLight : githubDark;
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img
-          src={heroImg}
-          className={styles.hero}
-          alt="Profile picture of Paramjeet Khatri"
-        />
-        <img
-          className={styles.colorMode}
-          src={themeIcon}
-          alt="Color mode icon"
-          onClick={toggleTheme}
-        />
+        <div className={styles.ring}></div>
+        <img src={heroImg} className={styles.centerImage} />
+        <div className={styles.colorMode} onClick={toggleTheme}>
+          <img
+            src={sun}
+            className={`${styles.themeIcon} ${
+              theme === "light" ? styles.visible : styles.hidden
+            }`}
+            alt="Light mode icon"
+          />
+          <img
+            src={moon}
+            className={`${styles.themeIcon} ${
+              theme === "dark" ? styles.visible : styles.hidden
+            }`}
+            alt="Dark mode icon"
+          />
+        </div>
       </div>
       <div className={styles.info}>
         <h1>
@@ -40,20 +46,36 @@ function Hero() {
           <br />
           Khatri
         </h1>
-        <h2>Full Stack Developer</h2>
+        <h2 className={styles.h2}>Full Stack Developer</h2>
         <span>
-          <a href="https://www.linkedin.com/in/paramjeet-khatri-00b43321b/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/paramjeet-khatri-00b43321b/"
+            target="_blank"
+          >
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
           <a href="https://github.com/DevDreamer-pk" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://www.instagram.com/paramjeet_khatri/?hl=en" target="_blank">
+          <a
+            href="https://www.instagram.com/paramjeet_khatri/?hl=en"
+            target="_blank"
+          >
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps and scalable Node.js solutions for commercial businesses
+        <Typewriter
+          words={[
+            "With a passion for developing modern React web apps and scalable Node.js solutions for commercial businesses."
+          ]}
+          loop={false}  // No need to loop if it's a single sentence
+          cursor
+          cursorStyle="_"
+          typeSpeed={40}  // Slightly slower for readability
+          deleteSpeed={20} 
+          delaySpeed={2000}  // Adds a pause before deleting (if loop is true)
+        />
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import styles from './ProjectsStyles.module.css';
+import styles from "./ProjectsStyles.module.css";
 import travalot from '../../assets/travalot.png';
 import zbrain from '../../assets/zbrain.png';
 import makeMyTale from '../../assets/makeMyTale(1).png';
@@ -8,67 +8,130 @@ import xdcPay from '../../assets/xdcPay.png';
 import ultrafan from '../../assets/ultrafan.png';
 import astropledge from '../../assets/astropledge.png';
 import xdc from '../../assets/xdc.png';
-import ProjectCard from '../../common/ProjectCard';
+// import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={travalot}
-          link="https://www.travalot.com/"
-          h3="Travalot"
-          p="Tourism website"
-        />
-        <ProjectCard
-          src={zbrain}
-          link="https://zbrain.ai/"
-          h3="Zbrain"
-          p="AI Agent"
-        />
-        <ProjectCard
-          src={makeMyTale}
-          link="https://makemytale.com/"
-          h3="Make My Tale"
-          p="AI Story Creation Platform"
-        />
-        <ProjectCard
-          src={shell}
-          link="#"
-          h3="Shell Global"
-          p="Shell AI Management System"
-        />
-          <ProjectCard
-          src={scortik}
-          link="https://scortik.com/"
-          h3="Scortik"
-          p="Blockchain Review Platform"
-        />
-        <ProjectCard
-          src={xdcPay}
-          link="https://chromewebstore.google.com/detail/xdcpay/bocpokimicclpaiekenaeelehdjllofo"
-          h3="XDC Pay"
-          p="Crypto Wallet Extension"
-        />
-        <ProjectCard
-          src={ultrafan}
-          link="https://app.fanblock.com/"
-          h3="Ultrafan"
-          p="Fan Engagement Platform"
-        />
-        <ProjectCard
-          src={astropledge}
-          link="https://astropledge.com/"
-          h3="Astropledge"
-          p="A Space Layer"
-        />
-        <ProjectCard
-          src={xdc}
-          link="https://github.com/XDCFoundation/XDC3Delphi"
-          h3="XDC SDK Delphi"
-          p="SDK for XDC Network"
-        />
+        <section id="card1" className={styles.card}>
+          <img
+            src={travalot}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Travalot</p>
+            <p className={styles.card__description}>
+            Travalot is a travel company that specializes in creating personalized holiday packages. Established in 2019 and based in Hyderabad, India
+            </p>
+          </div>
+        </section>
+        <section id="card2" className={styles.card}>
+          <img
+            src={zbrain}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Zbrain</p>
+            <p className={styles.card__description}>
+            ZBrain is a unified platform for enterprise AI enablement, seamlessly guiding organizations from AI readiness evaluation and opportunity to full-scale solution implementation.
+            </p>
+          </div>
+        </section>
+        <section id="card3" className={styles.card}>
+          <img
+            src={makeMyTale}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Make My Tale</p>
+            <p className={styles.card__description}>
+            Create custom stories with MakeMyTale, a story creation platform that generates unique plotlines based on personalized story prompts.
+            </p>
+          </div>
+        </section>
+        <section id="card4" className={styles.card}>
+          <img
+            src={shell}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Shell Global</p>
+            <p className={styles.card__description}>
+              Shell Global`s AI management platform helps them to drive innovation and growth.
+            </p>
+          </div>
+        </section>
+        <section id="card5" className={styles.card}>
+          <img
+            src={scortik}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Scortik</p>
+            <p className={styles.card__description}>
+            Scortik is blockchain protocols rating and review platform to enable Web3 developers to make the right choice.
+            </p>
+          </div>
+        </section>
+        <section id="card6" className={styles.card}>
+          <img
+            src={xdcPay}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>XDC Pay</p>
+            <p className={styles.card__description}>
+            XDCPay Browser Extension. The XDCPay is an extension for accessing XDC`s XDPoS enabled distributed applications, or `Dapps` in your browser!
+            </p>
+          </div>
+        </section>
+        <section id="card7" className={styles.card}>
+          <img
+            src={ultrafan}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Ultrafan FANBLOCK</p>
+            <p className={styles.card__description}>
+            FANBLOCK brings you a new, exciting game concept. Own blocks of your favourite team`s home ground and get closer to the club and game you love.
+            </p>
+          </div>
+        </section>
+        <section id="card8" className={styles.card}>
+          <img
+            src={astropledge}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>Astropledge</p>
+            <p className={styles.card__description}>
+            A Cybersecurity Space Layer to prevent and monitoring Cybercrime for Satellites by leveraging Smart Contracts and Blockchain Stack.
+            </p>
+          </div>
+        </section>
+        <section id="card9" className={styles.card}>
+          <img
+            src={xdc}  // Replace this with your image URL
+            alt="Project Image"
+            className={styles.card__image}
+          />
+          <div className={styles.card__content}>
+            <p className={styles.card__title}>XDC SDK Delphi</p>
+            <p className={styles.card__description}>
+            XDC3Delphi SDK with support for XRC20 and XRC721 smart contract.
+            </p>
+          </div>
+        </section>
       </div>
     </section>
   );
